afd94eaaa99601c9a7e28e4001e94049ada7b694
wzq <1228740123@qq.com>
2020-08-14 09:50:07 +0800

    longest_repeating_character_replacement solution2

--- src/main/java/com/leetcode/longest_repeating_character_replacement/Solution2.java
+++ src/main/java/com/leetcode/longest_repeating_character_replacement/Solution2.java
@@ -15,7 +15,34 @@ import com.ciaoshen.leetcode.util.*;
  * =============================================
  */
 class Solution2 implements Solution {
+    public int characterReplacement(String s, int k) {
+        if (s.length() <= k) {return s.length();}
 
-    
+        char[] chars = s.toCharArray();
+        int[] hash = new int[26];
+        int d_inx = 1;
 
+        int ret = 0, start = 0, maxCount = 0;
+
+        for (int end = 0; end < chars.length; end++) {
+
+            hash[chars[end] - 'A']++;
+
+            maxCount = Math.max(maxCount, hash[chars[end] - 'A']);
+
+            while(end - start + 1 - maxCount > k) {
+                hash[chars[start] - 'A']--;
+                start++;
+            }
+
+            ret = Math.max(ret, end - start + 1);
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        solution.characterReplacement("ABAB", 2);
+    }
 }
