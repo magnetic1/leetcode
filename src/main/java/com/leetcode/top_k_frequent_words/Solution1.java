ea9a1e9efc0f0e8553747e0f468790fc82780952
wzq <1228740123@qq.com>
2020-10-17 21:11:10 +0800

    top_k_frequent_words

--- src/main/java/com/leetcode/top_k_frequent_words/Solution1.java
+++ src/main/java/com/leetcode/top_k_frequent_words/Solution1.java
@@ -16,23 +16,25 @@ import com.ciaoshen.leetcode.util.*;
  */
 class Solution1 implements Solution {
 
-    Map<String, Integer> count = new HashMap<>();
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> count = new HashMap<>();
         for(String word: words) {
-        count.put(word, count.getOrDefault(word, 0) + 1);
-    }
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
 
-    PriorityQueue<String> heap = new PriorityQueue<>((w1, w2) ->
-            count.get(w1).equals(count.get(w2)) ? w2.compareTo(w1) : count.get(w1) - count.get(w2)
-    );
+        PriorityQueue<String> heap = new PriorityQueue<>((w1, w2) ->
+                count.get(w1).equals(count.get(w2)) ? w2.compareTo(w1) : count.get(w1) - count.get(w2)
+        );
 
         for (String word: count.keySet()) {
-        heap.offer(word);
-        if (heap.size() > k) heap.poll();
-    }
+            heap.offer(word);
+            if (heap.size() > k) heap.poll();
+        }
 
-    List<String> ans = new ArrayList();
+        List<String> ans = new ArrayList();
         while (!heap.isEmpty()) ans.add(heap.poll());
         Collections.reverse(ans);
         return ans;
+    }
 
 }
