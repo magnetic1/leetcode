918b33d2a4763a6b23db69850d472e137002ee03
wzq <1228740123@qq.com>
2020-10-26 18:58:11 +0800

    how_many_numbers_are_smaller_than_the_current_number

---
+++ src/main/java/com/leetcode/how_many_numbers_are_smaller_than_the_current_number/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - how_many_numbers_are_smaller_than_the_current_number
+ */
+package com.leetcode.how_many_numbers_are_smaller_than_the_current_number;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
