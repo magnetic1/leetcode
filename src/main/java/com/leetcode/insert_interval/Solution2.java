97bbc33db4b4a68eecd3d72a043f4e6ebd5a1edc
wzq <1228740123@qq.com>
2020-11-04 18:00:05 +0800

    insert-interval

--- src/main/java/com/leetcode/insert_interval/Solution2.java
+++ src/main/java/com/leetcode/insert_interval/Solution2.java
@@ -16,6 +16,38 @@ import com.ciaoshen.leetcode.util.*;
  */
 class Solution2 implements Solution {
 
-    
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int r = -1, l = intervals.length;
+        int min = newInterval[0], max = newInterval[1];
+
+        for(int i = 0; i < intervals.length; i++) {
+            if(intervals[i][1] < newInterval[0]) {
+                r = i;
+            } else if(intervals[i][0] > newInterval[1]) {
+                l = i;
+                break;
+            }
+        }
+
+        if(l - r < 2) {
+            int[][] new_intervals = new int[intervals.length + 1][2];
+            System.arraycopy(intervals, 0, new_intervals, 0, r + 1);
+            System.arraycopy(intervals, l, new_intervals, l + 1, intervals.length - l);
+            new_intervals[l][0] = min; new_intervals[l][1] = max;
+            return new_intervals;
+        } else {
+            min = Math.min(min, intervals[r + 1][0]);
+            max = Math.max(max, intervals[l - 1][1]);
+
+            int len = intervals.length - l + r + 2;
+
+            int[][] new_intervals = new int[len][2];
+            System.arraycopy(intervals, 0, new_intervals, 0, r + 1);
+            System.arraycopy(intervals, l, new_intervals, r + 2, intervals.length - l);
+            new_intervals[r+1][0] = min; new_intervals[r+1][1] = max;
+            return new_intervals;
+        }
+
+    }
 
 }
