8862578f6b387559ca945e2de7165282ee513b30
wzq <1228740123@qq.com>
2020-11-25 22:02:31 +0800

    increasing_decreasing_string

---
+++ src/main/java/com/leetcode/increasing_decreasing_string/Solution1.java
@@ -0,0 +1,43 @@
+/**
+ * Leetcode - increasing_decreasing_string
+ */
+package com.leetcode.increasing_decreasing_string;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public String sortString(String s) {
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        while (ret.length() < s.length()) {
+            for (int i = 0; i < 26; i++) {
+                if (num[i] > 0) {
+                    ret.append((char) (i + 'a'));
+                    num[i]--;
+                }
+            }
+            for (int i = 25; i >= 0; i--) {
+                if (num[i] > 0) {
+                    ret.append((char) (i + 'a'));
+                    num[i]--;
+                }
+            }
+        }
+        return ret.toString();
+    }
+
+}
