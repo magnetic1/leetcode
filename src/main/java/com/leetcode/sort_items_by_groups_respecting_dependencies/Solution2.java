532a2ebd7006d0f559bb53e852139688e5e627a3
wzq <1228740123@qq.com>
2021-01-12 22:44:17 +0800

    sort_items_by_groups_respecting_dependencies

---
+++ src/main/java/com/leetcode/sort_items_by_groups_respecting_dependencies/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Leetcode - sort_items_by_groups_respecting_dependencies
+ */
+package com.leetcode.sort_items_by_groups_respecting_dependencies;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
