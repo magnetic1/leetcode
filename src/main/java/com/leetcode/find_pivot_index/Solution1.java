911059f537a56fad52d3bcb099be2247bcedceec
wzq <1228740123@qq.com>
2021-01-28 16:30:12 +0800

    find_pivot_index

---
+++ src/main/java/com/leetcode/find_pivot_index/Solution1.java
@@ -0,0 +1,34 @@
+/**
+ * Leetcode - find_pivot_index
+ */
+package com.leetcode.find_pivot_index;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for(int num : nums) {
+            total += num;
+        }
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+
+}
