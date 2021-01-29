2ef9b2ef16f956718e2d4aa0f4f297a84838cc74
wzq <1228740123@qq.com>
2021-01-29 20:00:29 +0800

    path_with_minimum_effort

---
+++ src/main/java/com/leetcode/path_with_minimum_effort/Solution1.java
@@ -0,0 +1,94 @@
+/**
+ * Leetcode - path_with_minimum_effort
+ */
+package com.leetcode.path_with_minimum_effort;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length, n = heights[0].length;
+
+        List<int[]> edges = new ArrayList<int[]>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int id = i * n + j;
+                if (i > 0) {
+                    edges.add(new int[]{id - n, id, Math.abs(heights[i][j] - heights[i - 1][j])});
+                }
+                if (j > 0) {
+                    edges.add(new int[]{id - 1, id, Math.abs(heights[i][j] - heights[i][j - 1])});
+                }
+            }
+        }
+        edges.sort(Comparator.comparingInt(edge -> edge[2]));
+
+        UnionFind uf = new UnionFind(m * n);
+        for(int[] edge : edges) {
+            int cost = edge[2];
+            uf.unite(edge[0], edge[1]);
+            if(uf.connected(0, m * n -1)) {
+                return cost;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// 并查集模板
+class UnionFind {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFind(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+}
---
+++ src/main/java/com/leetcode/path_with_minimum_effort/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Leetcode - path_with_minimum_effort
+ */
+package com.leetcode.path_with_minimum_effort;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
