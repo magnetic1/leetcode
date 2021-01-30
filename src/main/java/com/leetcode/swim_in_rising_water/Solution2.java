e5cc92b6905972431b05cf565f2051fcd545752d
wzq <1228740123@qq.com>
2021-01-30 14:33:56 +0800

    swim-in-rising-water

--- src/main/java/com/leetcode/swim_in_rising_water/Solution2.java
+++ src/main/java/com/leetcode/swim_in_rising_water/Solution2.java
@@ -2,20 +2,95 @@
  * Leetcode - swim_in_rising_water
  */
 package com.leetcode.swim_in_rising_water;
+
 import java.util.*;
+
 import com.ciaoshen.leetcode.util.*;
 
-/** 
+/**
  * log instance is defined in Solution interface
  * this is how slf4j will work in this class:
  * =============================================
- *     if (log.isDebugEnabled()) {
- *         log.debug("a + b = {}", sum);
- *     }
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
  * =============================================
  */
 class Solution2 implements Solution {
 
-    
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
 
+        List<int[]> edges = new ArrayList<int[]>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int id = i * n + j;
+                if (i > 0) {
+                    edges.add(new int[]{id - n, id, Math.max(grid[i][j], grid[i - 1][j])});
+                }
+                if (j > 0) {
+                    edges.add(new int[]{id - 1, id, Math.max(grid[i][j], grid[i][j - 1])});
+                }
+            }
+        }
+        edges.sort(Comparator.comparingInt(edge -> edge[2]));
+
+        UnionFind uf = new UnionFind(n * n);
+        for (int[] edge : edges) {
+            int cost = edge[2];
+            uf.unite(edge[0], edge[1]);
+            if (uf.connected(0, n * n - 1)) {
+                return cost;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// 并查集模板
+class UnionFind {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFind(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
 }
