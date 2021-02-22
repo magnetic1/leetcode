e70bd2516c766619d98fd163acba107cc56d0270
wzq <1228740123@qq.com>
2021-02-22 19:45:06 +0800

    longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit

--- src/main/java/com/leetcode/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit/Solution2.java
+++ src/main/java/com/leetcode/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit/Solution2.java
@@ -16,6 +16,36 @@ import com.ciaoshen.leetcode.util.*;
  */
 class Solution2 implements Solution {
 
-    
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> queMin = new LinkedList<>();
+        Deque<Integer> queMax = new LinkedList<>();
+        int left = 0, right = 0, n = nums.length;
+
+        int ret = 0;
+        while(right < n) {
+            int num = nums[right];
+            while(!queMax.isEmpty() && queMax.peek() < num) {
+                queMax.pop();
+            }
+            queMax.push(num);
+            while(!queMin.isEmpty() && queMin.peek() > num) {
+                queMin.pop();
+            }
+            queMin.push(num);
+
+            if(queMax.peekLast() - queMin.peekLast() > limit) {
+                if(queMax.peekLast() == nums[left]) {
+                    queMax.pollLast();
+                }
+                if(queMin.peekLast() == nums[left]) {
+                    queMin.pollLast();
+                }
+                left++;
+            }
+            right++;
+        }
+
+        return right - left;
+    }
 
 }
