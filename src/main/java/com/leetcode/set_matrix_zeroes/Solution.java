79281b650f13f6b898e8c6fc04c9d231ee90b565
wzq <1228740123@qq.com>
2021-03-21 16:34:32 +0800

    set_matrix_zeroes

---
+++ src/main/java/com/leetcode/set_matrix_zeroes/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - set_matrix_zeroes
+ */
+package com.leetcode.set_matrix_zeroes;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/set_matrix_zeroes/Solution1.java
@@ -0,0 +1,43 @@
+/**
+ * Leetcode - set_matrix_zeroes
+ */
+package com.leetcode.set_matrix_zeroes;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+}
