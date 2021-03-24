7a81e7d3fba42cb180c804ee5fd2a366d9c9633a
wzq <1228740123@qq.com>
2021-03-24 21:24:15 +0800

    132_pattern

---
+++ src/main/java/com/leetcode/132_pattern/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - 132_pattern
+ */
+package com.leetcode.132_pattern;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
