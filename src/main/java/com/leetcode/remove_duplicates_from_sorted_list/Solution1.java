dde1a92fa730c0f7e110193023d22687d59a0034
wzq <1228740123@qq.com>
2021-03-26 15:32:45 +0800

    remove_duplicates_from_sorted_list

---
+++ src/main/java/com/leetcode/remove_duplicates_from_sorted_list/Solution1.java
@@ -0,0 +1,36 @@
+/**
+ * Leetcode - remove_duplicates_from_sorted_list
+ */
+package com.leetcode.remove_duplicates_from_sorted_list;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+
+        return head;
+    }
+
+}
