d8041ff4c280b8504c7239962b58bb2dbc7faf33
wzq <1228740123@qq.com>
2021-03-31 14:54:16 +0800

    List<List<Integer>> ans = new ArrayList<>();

--- src/main/java/com/leetcode/subsets_ii/Solution2.java
+++ src/main/java/com/leetcode/subsets_ii/Solution2.java
@@ -16,6 +16,41 @@ import com.ciaoshen.leetcode.util.*;
  */
 class Solution2 implements Solution {
 
-    
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        ans.add(new ArrayList<>());
+
+        for(int i = 1; i <= nums.length; i++) {
+            search(nums, 0, i, new ArrayList<>());
+        }
+
+        return ans;
+    }
+
+    void search(int[] nums, int i, int len, List<Integer> list) {
+        int n = nums.length;
+
+        if(n < i + len) {
+            return;
+        }
+
+        if(len == 0) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        int value = nums[i] - 1;
+        for(; i < n; i++) {
+            if(nums[i] != value) {
+                list.add(nums[i]);
+                search(nums, i + 1, len - 1, list);
+                list.remove(list.size() - 1);
+                value = nums[i];
+            }
+        }
+
+    }
 
 }
