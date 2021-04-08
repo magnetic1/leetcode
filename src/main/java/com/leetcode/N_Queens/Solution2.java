4c33f21f7a7ecbfe5d3fd64e6314ab208b9f0075
wzq <1228740123@qq.com>
2021-04-08 23:13:24 +0800

    find_minimum_in_rotated_sorted_array

--- src/main/java/com/leetcode/N_Queens/Solution2.java
+++ src/main/java/com/leetcode/N_Queens/Solution2.java
@@ -15,7 +15,54 @@ import com.ciaoshen.leetcode.util.*;
  * =============================================
  */
 class Solution2 implements Solution {
+    List<List<String>> ans = new ArrayList<>();
 
-    
+    public List<List<String>> solveNQueens(int n) {
+        boolean[] columns = new boolean[n];
+        boolean[] diagonals1 = new boolean[2 * n];
+        boolean[] diagonals2 = new boolean[2 * n];
 
+
+        int[] sample = new int[n];
+
+        search(0, n, sample, columns, diagonals1, diagonals2);
+
+        return ans;
+    }
+
+    void search(int row, int n, int[] sample, boolean[] columns, boolean[] diagonals1, boolean[] diagonals2) {
+        if (row == n) {
+            List<String> list = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < n; j++) {
+                    if (sample[i] == j) {
+                        sb.append('Q');
+                    } else {
+                        sb.append('.');
+                    }
+                }
+                list.add(sb.toString());
+            }
+            ans.add(list);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (columns[col] || diagonals1[row - col + n] || diagonals2[row + col]) {
+                continue;
+            }
+            sample[row] = col;
+
+            columns[col] = true;
+            diagonals1[row - col + n] = true;
+            diagonals2[row + col] = true;
+
+            search(row + 1, n, sample, columns, diagonals1, diagonals2);
+
+            columns[col] = false;
+            diagonals1[row - col + n] = false;
+            diagonals2[row + col] = false;
+        }
+    }
 }
