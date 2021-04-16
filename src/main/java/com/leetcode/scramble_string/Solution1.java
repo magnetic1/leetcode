e55889f5bb73694d7a1e0f694d3c67b3d10fc60c
wzq <1228740123@qq.com>
2021-04-16 20:09:18 +0800

    scramble_string

---
+++ src/main/java/com/leetcode/scramble_string/Solution1.java
@@ -0,0 +1,79 @@
+/**
+ * Leetcode - scramble_string
+ */
+package com.leetcode.scramble_string;
+
+import java.util.*;
+
+import com.ciaoshen.leetcode.util.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution1 implements Solution {
+    int[][][] memo;
+    String s1, s2;
+
+    public boolean isScramble(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+        int length = s1.length();
+        memo = new int[length][length][length + 1];
+        return dfs(0, 0, length);
+    }
+
+    private boolean dfs(int i1, int i2, int length) {
+        if (memo[i1][i2][length] != 0) {
+            return memo[i1][i2][length] == 1;
+        }
+
+        if (!checkIfSimilar(i1, i2, length)) {
+            memo[i1][i2][length] = -1;
+            return false;
+        }
+
+        if (length == 1) {
+            return true;
+        }
+
+        for (int i = 1; i < length; i++) {
+            if (dfs(i1, i2, i) && dfs(i1 + i, i2 + i, length - i)) {
+                memo[i1][i2][length] = 1;
+                return true;
+            }
+
+            if (dfs(i1, i2 + length - i, i) && dfs(i1 + i, i2, length - i)) {
+                memo[i1][i2][length] = 1;
+                return true;
+            }
+        }
+
+        memo[i1][i2][length] = -1;
+        return false;
+    }
+
+    public boolean checkIfSimilar(int i1, int i2, int length) {
+        Map<Character, Integer> freq = new HashMap<Character, Integer>();
+        for (int i = i1; i < i1 + length; ++i) {
+            char c = s1.charAt(i);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        for (int i = i2; i < i2 + length; ++i) {
+            char c = s2.charAt(i);
+            freq.put(c, freq.getOrDefault(c, 0) - 1);
+        }
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            int value = entry.getValue();
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/leetcode/scramble_string/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Leetcode - scramble_string
+ */
+package com.leetcode.scramble_string;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
