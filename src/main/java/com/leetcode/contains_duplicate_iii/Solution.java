9c0cc0fd60043873d4ec92bdcd216559fb42d7b4
wzq <1228740123@qq.com>
2021-04-19 14:28:33 +0800

    contains_duplicate_iii

---
+++ src/main/java/com/leetcode/contains_duplicate_iii/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - contains_duplicate_iii
+ */
+package com.leetcode.contains_duplicate_iii;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/contains_duplicate_iii/Solution1.java
@@ -0,0 +1,43 @@
+/**
+ * Leetcode - contains_duplicate_iii
+ */
+package com.leetcode.contains_duplicate_iii;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        int n = nums.length;
+        TreeSet<Long> set = new TreeSet<Long>();
+        for (int i = 0; i < n; i++) {
+            Long ceiling = set.ceiling((long) nums[i] - (long) t);
+            if (ceiling != null && ceiling <= (long) nums[i] + (long) t) {
+                return true;
+            }
+            set.add((long) nums[i]);
+            if (i >= k) {
+                set.remove((long) nums[i - k]);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution1 solution = new Solution1();
+        System.out.println(solution.containsNearbyAlmostDuplicate(
+                new int[]{2147483647, -1, 2147483647},
+                1,
+                2147483647
+        ));
+    }
+}
