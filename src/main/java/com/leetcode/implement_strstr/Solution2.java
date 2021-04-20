086090386a8383ad08832cecd86e0332128a17e1
wzq <1228740123@qq.com>
2021-04-20 19:12:52 +0800

    implement-strstr

--- src/main/java/com/leetcode/implement_strstr/Solution2.java
+++ src/main/java/com/leetcode/implement_strstr/Solution2.java
@@ -16,6 +16,42 @@ import com.ciaoshen.leetcode.util.*;
  */
 class Solution2 implements Solution {
 
-    
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length()) {
+            return -1;
+        }
+        if (needle.length() == 0) {
+            return 0;
+        }
+
+        int m = needle.length();
+        int n = haystack.length();
+
+        int[] next = new int[m];
+        next[0] = -1;
+        for (int i = 0, j = -1; i < m - 1; ) {
+            if (j == -1 || needle.charAt(i) == needle.charAt(j)) {
+                j++;
+                i++;
+                next[i] = j;
+            } else {
+                j = next[j];
+            }
+        }
+
+        for (int i = 0, j = 0; i < n; ) {
+            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+
+            if (j == m) {
+                return i - m;
+            }
+        }
+        return -1;
+    }
 
 }
